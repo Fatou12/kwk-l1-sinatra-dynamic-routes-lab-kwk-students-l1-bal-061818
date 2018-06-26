c8470057b5065aefd3fd63afee2a8180b7f2cb0c
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-26 19:41:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
-end
+
+  get '/ reversename/ :name' do
+    params [:name].reverse
+  end
+  
+get '/ square/ : number' do
